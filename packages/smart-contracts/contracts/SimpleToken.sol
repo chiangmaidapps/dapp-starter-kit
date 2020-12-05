2c82c53c4d86976e0cd3ddc6bfc461d4216385b2
Chris Piatt <chris@blocklytics.org>
2020-12-05 23:07:55 +0700

    added simple token example

---
+++ packages/smart-contracts/contracts/SimpleToken.sol
@@ -0,0 +1,144 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.7.0;
+
+import "./lib/SafeMath.sol";
+
+/**
+ * @title SimpleToken
+ * @dev Simple ERC-20 Token
+ */
+contract SimpleToken {
+    using SafeMath for uint256;
+
+   /// @notice EIP-20 token name for this token
+    string public name;
+
+    /// @notice EIP-20 token symbol for this token
+    string public symbol;
+
+    /// @notice EIP-20 token decimals for this token
+    uint8 public constant decimals = 18;
+
+    /// @notice Total number of tokens in circulation
+    uint256 public totalSupply = 100_000_000e18; // 100 million
+
+    /// @dev Allowance amounts on behalf of others
+    mapping (address => mapping (address => uint256)) internal allowances;
+
+    /// @dev Official record of token balances for each account
+    mapping (address => uint256) internal balances;
+
+    /// @notice The standard EIP-20 transfer event
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /// @notice The standard EIP-20 approval event
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @notice Construct a new Simple Token
+     * @param _name Name of the token
+     * @param _symbol Symbol of the token
+     */
+    constructor(string memory _name, string memory _symbol) {
+      name = _name;
+      symbol = _symbol;
+    }
+
+    /**
+     * @notice Get the number of tokens held by the `account`
+     * @param account The address of the account to get the balance of
+     * @return The number of tokens held
+     */
+    function balanceOf(address account) external view returns (uint) {
+        return balances[account];
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `msg.sender` to `dst`
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transfer(address dst, uint256 amount) external returns (bool) {
+        _transferTokens(msg.sender, dst, amount);
+        return true;
+    }
+
+    /**
+     * @notice Transfer `amount` tokens from `src` to `dst`
+     * @param src The address of the source account
+     * @param dst The address of the destination account
+     * @param amount The number of tokens to transfer
+     * @return Whether or not the transfer succeeded
+     */
+    function transferFrom(address src, address dst, uint256 amount) external returns (bool) {
+        address spender = msg.sender;
+        uint256 spenderAllowance = allowances[src][spender];
+
+        if (spender != src && spenderAllowance != uint256(-1)) {
+            uint256 newAllowance = spenderAllowance.sub(
+                amount,
+                "transfer amount exceeds allowance"
+            );
+            allowances[src][spender] = newAllowance;
+
+            emit Approval(src, spender, newAllowance);
+        }
+
+        _transferTokens(src, dst, amount);
+        return true;
+    }
+
+    /**
+     * @notice Get the number of tokens `spender` is approved to spend on behalf of `account`
+     * @param account The address of the account holding the funds
+     * @param spender The address of the account spending the funds
+     * @return The number of tokens approved
+     */
+    function allowance(address account, address spender) external view returns (uint) {
+        return allowances[account][spender];
+    }
+
+    /**
+     * @notice Approve `spender` to transfer up to `amount` from `src`
+     * @dev This will overwrite the approval amount for `spender`
+     * and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     * @return Whether or not the approval succeeded
+     */
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    /**
+     * @notice Transfer implementation
+     * @param from The address of the account which owns tokens
+     * @param to The address of the account which is receiving tokens
+     * @param value The number of tokens that are being transferred
+     */
+    function _transferTokens(address from, address to, uint256 value) internal {
+        require(to != address(0), "cannot transfer to the zero address");
+
+        balances[from] = balances[from].sub(
+            value,
+            "transfer exceeds from balance"
+        );
+        balances[to] = balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    /**
+     * @notice Approval implementation
+     * @param owner The address of the account which owns tokens
+     * @param spender The address of the account which may transfer tokens
+     * @param amount The number of tokens that are approved (2^256-1 means infinite)
+     */
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "cannot approve from the zero address");
+        require(spender != address(0), "cannot approve to the zero address");
+        allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+}
