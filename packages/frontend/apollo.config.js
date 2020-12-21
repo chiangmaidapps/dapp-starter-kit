b6971f6e3d924c9571dacdd0c354ae17cf4127f5
Pyython <vogonpoetry06@gmail.com>
2020-12-21 21:01:03 +0700

    moved settings

--- packages/frontend/apollo.config.js
+++ packages/frontend/apollo.config.js
@@ -1,10 +1,6 @@
-import {
-  VUE_APP_SUBGRAPH_NAME
-} from "./settings";
-
 module.exports = {
   client: {
-    service: VUE_APP_SUBGRAPH_NAME,
+    service: process.env.VUE_APP_SUBGRAPH_NAME,
     localSchemaFile: '../subgraph/schema.graphql'
   }
 }
